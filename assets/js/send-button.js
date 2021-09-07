e266711e44c36710e97de071b6ead9ea75ae9a65
AndrewH1188 <andrewh1188@gmail.com>
2021-09-07 11:29:46 +0000

    Added JS message to log on console and display a message to the user when the send button has been clicked.

---
+++ assets/js/send-button.js
@@ -0,0 +1,5 @@
+function myFunction() {
+    documentGetElementById(send).innerHTML = "Thank you for your enquiry, we have recieved your enquiry and will reply to you soon";
+}
+
+console.log("Submitted Successfully");
