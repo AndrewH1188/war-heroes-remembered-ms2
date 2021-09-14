aa1b26c420f646b3b03a7604d6efc0f3e64f25aa
AndrewH1188 <andrewh1188@gmail.com>
2021-09-14 23:31:32 +0000

    With much perseverance the sendEmail.js functions and I am able to recieve emails from my website.

---
+++ assets/js/sendEmail.js
@@ -0,0 +1,16 @@
+// Code from Rosie's Resume and slightly amended
+function sendMail(contactForm) {
+    emailjs.send("service_ny0myoh","template_djem23l",{
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "website_enquiry": contactForm.enquirytype.value,
+    })
+
+    .then(function() {
+        console.log('SUCCESS!');
+    }, function(error) {
+        console.log('FAILED...', error);
+    }
+    );
+    return false;  // To block from loading a new page
+}
