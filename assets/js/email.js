ee15efbe42cf5af2e5e623ea58f3516c158010f9
AndrewH1188 <andrewh1188@gmail.com>
2021-09-14 00:54:31 +0000

    Working through these email.js and send-button.js as there were errors, I will look at these again tomorrow and see if I can get these working.

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,4 +1,16 @@
-
-// function () {
-//    email.js.init("user_service_5dblp87");
-// }
+function sendMail(contactForm) {
+    emailjs.send("service_5dblp87", "template_nbqpspj", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "project_request": contactForm.projectsummary.value
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        }
+    );
+    return false;  // To block from loading a new page
+}
--- assets/js/send-button.js
+++ assets/js/send-button.js
@@ -1,5 +1,3 @@
 function myFunction() {
-    documentGetElementById(send).innerHTML = "Thank you for your enquiry, we have recieved your enquiry and will reply to you soon";
+   documentGetElementById(send).innerHTML = "Thank you for your enquiry, we have recieved your enquiry and will reply to you soon";
 }
-
-console.log("Send was successful");
