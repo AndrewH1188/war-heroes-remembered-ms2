ea4bd38f6190cd5112b8aaebe8960a579facc658
AndrewH1188 <andrewh1188@gmail.com>
2021-09-14 00:52:53 +0000

    Working with Sheryl and Michael in Tutor Support the map now displays, has markers and InfoWindows.

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,27 +1,149 @@
-function initMap() {
-  var map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 11,
-      center: {lat: 51.40219, lng: -0.16890}
-    });
+function LoadMap() {
+  const cemetery = { lat: 50.05484, lng: 3.52440 };
+  const map = new google.maps.Map(document.getElementById("map"), {
+  zoom: 5,
+  center: { lat: 50.05484, lng: 3.52440 },
+  });
+ 
+var markers = [
+  {
+      "title": 'Private Richard Spearink',
+      "lat": '50.054870024722895',
+      "lng": '3.5244262134913344',
+      "description": 'Private Richard Spearink <br> St Souplet British Cemetery'
+  },
+  {
+      "title": 'Private Frederick Spearink',
+      "lat": '51.402170802739015', 
+      "lng": '-0.16887147356538348',
+      "description": 'Private Frederick Spearink <br> Mitcham War Memorial'
+  },
+  {
+    "title": 'Private Frederick Spearink',
+    "lat": '40.046079676342764', 
+    "lng": '26.179177425981795',
+    "description": 'Private Frederick Spearink <br> Helles Memorial' 
+  },
+  {
+      "title": 'Lance Sergeant Thomas Banister',
+      "lat": '50.63119698236488',
+      "lng": '2.8360939264008294', 
+      "description": 'Lance Sergeant Thomas Banister <br> Rue-Petillon Military Cemetery'
+  },
+  {
+      "title": 'Second Lieutenant Sidney Vandyke Hasluck',
+      "lat": '40.046079676342764', 
+      "lng": '26.179177425981795',
+      "description": 'Second Lieutenant Sidney Vandyke Hasluck <br> Helles Memorial'
+  },
+  {
+    "title": 'Nurse Elizabeth McMath (Daisy) Warnock',
+    "lat": '49.4093872940864',
+    "lng": '1.0655323414781486',
+    "description": 'Nurse Elizabeth McMath (Daisy) Warnock <br> St. Sever Cemetery'
+  },  
+  {
+    "title": 'Private Thomas Phillips',
+    "lat": '53.127595371367754',
+    "lng": '-1.295838617665544',
+    "description": 'Private Thomas Phillips <br> Huthwaite Cenotaph'
+  }, 
+  {
+    "title": 'Private Thomas Phillips',
+    "lat": '50.852156240636816',
+    "lng": '2.89112186873956',
+    "description": 'Private Thomas Phillips <br> Menin Gate Memorial'
+  },
+  {
+    "title": 'Leading Seaman John Sexton',
+    "lat": '50.36582785459549',
+    "lng": '-4.142284760117049',
+    "description": 'Leading Seaman John Sexton <br> Plymouth Naval Memorial'
+  },
+  {
+    "title": 'Lance Corporal Arthur Norman (Barney) Allen',
+    "lat": '50.852156240636816',
+    "lng": '2.89112186873956',
+    "description": 'Lance Corporal Arthur Norman (Barney) Allen <br> Menin Gate Memorial'
+  },
+  {
+    "title": 'Major and acting Lieutenant-Colonel Harry Moorhouse',
+    "lat": '50.88730239126956',
+    "lng": '2.998478924563846',
+    "description": 'Major and acting Lieutenant-Colonel Harry Moorhouse <br> Tyne Cot Memorial'
+  },
+  {
+    "title": 'Captain Ronald Moorhouse',
+    "lat": '50.88730239126956',
+    "lng": '2.998478924563846',
+    "description": 'Captain Ronald Moorhouse <br> Tyne Cot Memorial'
+  },
+  {
+    "title": 'Private John “Jack” Henry Thomas',
+    "lat": '50.37981021533251',
+    "lng": '2.7739841263899008',
+    "description": 'Private John “Jack” Henry Thomas <br> Canadian National Vimy Memorial'
+  },
+  {
+    "title": 'Private John “Jack” Henry Thomas',
+    "lat": '50.45154804442785',
+    "lng": '2.798063726393039',
+    "description": 'Private John “Jack” Henry Thomas <br> Loos British Cemetery'
+  },
+  {
+    "title": 'Private Bertram Alec Reader',
+    "lat": '50.05252071122409',
+    "lng": '2.687815630628548',
+    "description": 'Private Bertram Alec Reader <br> Thiepval Memorial Pier'
+  },
+  {
+    "title": 'Trooper George Ernest Dawkes',
+    "lat": '45.813590761971845',
+    "lng": '12.3477515531867',
+    "description": 'Trooper George Ernest Dawkes <br> Tezze British Cemetery'
+  },
+  {
+    "title": 'Private Adolphus Jack',
+    "lat": '50.933846037599',
+    "lng": '-1.4310416429023036',
+    "description": 'Private Adolphus Jack <br> Hollybrook Memorial'
+  },
+  {
+    "title": 'Lance Corporal Norman Frank Ewart Cole',
+    "lat": '50.05252071122409',
+    "lng": '2.687815630628548',
+    "description": 'Lance Corporal Norman Frank Ewart Cole <br> Thiepval Memorial Pier'
+  },
+  {
+    "title": 'Rifleman Henry William Longman',
+    "lat": '50.88730239126956',
+    "lng": '2.998478924563846',
+    "description": 'Rifleman Henry William Longman <br> Tyne Cot Memorial'
+  },
+  ];
+  window.onload = function () {
+    LoadMap();
+}
+
+      //Create and open InfoWindow.
+      var infoWindow = new google.maps.InfoWindow();
+
+      for (var i = 0; i < markers.length; i++) {
+          var data = markers[i];
+          var myLatlng = new google.maps.LatLng(data.lat, data.lng);
+          var marker = new google.maps.Marker({
+              position: myLatlng,
+              map: map,
+              title: data.title
+          });
+
+          //Attach click event to the marker.
+          (function (marker, data) {
+              google.maps.event.addListener(marker, "click", function (e) {
+                  //Wrap the content inside an HTML DIV in order to set height and width of InfoWindow.
+                  infoWindow.setContent("<div style = 'width:200px;min-height:40px'>" + data.description + "</div>");
+                  infoWindow.open(map, marker);
+              });
+          })(marker, data);
+      }
   }
-
-  //Coordinates for the various Memorials and Cemetries listed for the corrosponding Hero that will be marked on the map.
-// I used https://codepen.io/ahmadawais/pen/NQdWQx?editors=1010 to see how to do the below with the locations as this way it will show with the names of the memorials or cemetries
-var locations = [
-  ['Private Frederick Spearink - Mitcham War Memorial', 51.40219, -0.16890, 1],
-  ['Second Lieutenant Sidney Vandyke Hasluck and Private Frederick Spearink - Helles Memorial', 40.04597, 26.17921, 2],
-  ['Leading Seaman John Sexton - Plymouth Naval Memorial', 50.36571, 4.14228, 3],
-  ['Lance Corporal Arthur Norman (Barney) Allen and Private Thomas Phillips - Menin Gate Memorial', 50.85217, 2.89107, 4],
-  ['Private John “Jack” Henry Thomas - Canadian National Vimy Memorial', 50.397976, 2.77402, 5],
-  ['Private Bertram Alec Reader and Lance Corporal Norman Frank Ewart Cole - Thiepval Memorial Pier', 50.05069, 2.68577, 6],
-  ['Private Thomas Phillips - Huthwaite Cenotaph', 53.12764, -1.29581, 7],
-  ['Private Richard Spearink - St Souplet British Cemetery', 50.05484, 3.52440, 8],
-  ['Lance Sergeant Thomas Banister - Rue-Petillon Military Cemetery', 50.63123, 2.83609, 9],
-  ['Nurse Elizabeth McMath (Daisy) Warnock - St. Sever Cemetery', 49.40933, 1.06558, 10],
-  ['Major and acting Lieutenant-Colonel Harry Moorhouse, Captain Ronald Moorhouse and Rifleman Henry William Longman - Tyne Cot Memorial', 50.88725, 2.99852, 11],
-  ['Private John “Jack” Henry Thomas - Loos British Cemetery', 50.45156, 2.79803, 12],
-  ['Trooper George Ernest Dawkes - Tezze British Cemetery', 45.81382, 12.34771, 13],
-  ['Private Adolphus Jack - Hollybrook Memorial', 50.93371, -1.43099, 14]
-];
-
-// for the marker image (hopefully) assets/images/poppy-cross.png
