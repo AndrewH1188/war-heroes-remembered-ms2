a7353040394fd761368be1f5ccd39c90743c9cc2
AndrewH1188 <andrewh1188@gmail.com>
2021-09-15 19:52:14 +0000

    New files that work for the Heroes page, this condenses it down and makes the page interactive as all heroes appear on one page when a button is clicked.

---
+++ assets/js/heroes.js
@@ -0,0 +1,7 @@
+// JS from here: https://www.demo2s.com/javascript/jquery-toggle-show-hide-method.html
+
+$(document).ready(function(){
+  $("button").click(function(){
+    $("article").toggle();
+  });
+});
