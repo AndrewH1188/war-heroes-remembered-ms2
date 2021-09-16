773f4cfe0761db169236e46f38472f4ae6483ed3
AndrewH1188 <andrewh1188@gmail.com>
2021-09-16 17:45:49 +0000

    With help from Stack Overflow the button text changes on the Heroes page when clicked.

--- assets/js/heroes.js
+++ assets/js/heroes.js
@@ -4,3 +4,19 @@ $(document).ready(function(){
     $("article").toggle();
   });
 });
+
+
+// The code below taken from Stack Overflow from same issue from someone else's problem.
+
+//this will execute on page load(to be more specific when document ready event occurs)
+if ($('.ty-compact-list').length > 3) {
+  $('.ty-compact-list:gt(2)').hide();
+  $('.btn-heroes-pages').show();
+}
+
+$('.btn-heroes-pages').on('click', function() {
+  //toggle elements with class .ty-compact-list that their index is bigger than 2
+  $('.ty-compact-list:gt(2)').toggle();
+  //change text of show more element just for demonstration purposes to this demo
+  $(this).text() === 'Show less' ? $(this).text('Show more') : $(this).text('Show less');
+});
