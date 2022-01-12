e0b63905ca1c7c64ec5072c21240334108b2de66
AndrewH1188 <andrewh1188@gmail.com>
2022-01-12 00:41:12 +0000

    Fixing the contact form validation

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,15 +1,21 @@
 // Code from Rosie's Resume and slightly amended
-function sendMail(contactForm) {
-    emailjs.send("service_ny0myoh","template_djem23l", {
-        "from_name": contactForm.name.value,
-        "from_email": contactForm.emailaddress.value,
-        "website_enquiry": contactForm.enquirytype.value,
-    })
+let contactForm = document.getElementById("contactForm")
+contactForm.addEventListener("submit", function (e) {
+    e.preventDefault()
+    sendMail()
+})
 
-    .then(function() {
-        console.log('SUCCESS!');
-    }, function(error) {
-        console.log('FAILED...', error);
-    }
-    );
+function sendMail() {
+    emailjs.send("service_ny0myoh", "template_djem23l", {
+            "from_name": contactForm.name.value,
+            "from_email": contactForm.emailaddress.value,
+            "website_enquiry": contactForm.enquirytype.value,
+        })
+        .then(function () {
+            console.log('SUCCESS!');
+            alert('Thank you for contacting us, we have recieved your enquiry and will reply to you soon')
+        }, function (error) {
+            console.log('FAILED...', error);
+            alert('We had a problem with sending your message, please try again.')
+        });
 }
